f6a57c0eeeeef43b9f14f6bae8c42deb5a7cb79a
Chandan Mali <88288584+Chandanmali@users.noreply.github.com>
2022-07-22 14:05:00 +0530

    Update piano.js

--- piano.js
+++ piano.js
@@ -16,10 +16,7 @@ let happyBirthday = `G4,G4,A4,,G4,,C5,,B4,,,,
                      G4,G4,A4,,G4,,D5,,C5,,,,
                      G4,G4,G5,,E5,,C5,,B4,,A4,,
                      F5,F5,E5,,C5,,D5,,C5,,,,`;
-let saregama = `G4,G4,A4,,G4,,C5,,B4,,,,
-                     
-                     G4,G4,G5,,E5,,C5,,B4,,A4,,
-                     F5,F5,E5,,C5,,D5,,C5,,,,`;
+
 
 let song = (notesString, tempo = 2, cb) => {
   let notes = notesString.split(",");
